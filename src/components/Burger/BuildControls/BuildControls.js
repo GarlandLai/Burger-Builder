e82efcf2cfb1db3114a3e26d17a7632b78124dbf
Garland Lai <garlandlai24@gmail.com>
2020-05-27 11:45:31 -0700

    add BuildContols, but still need to figure out why styling is not working on buttons

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const buildControls = (props) => <div></div>;
-export default burgerControls;
+const controls = [
+	{ label: 'Salad', type: 'salad' },
+	{ label: 'Bacon', type: 'bacon' },
+	{ label: 'Cheese', type: 'Cheese' },
+	{ label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+	<div className={classes.BuildControls}>
+		{controls.map((ctrl) => (
+			<BuildControl key={ctrl.label} label={ctrl.label} />
+		))}
+	</div>
+);
+
+export default buildControls;
