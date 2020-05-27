1db5d1c3ed1f689a4fcd7652217d58cfb9f3da34
Garland Lai <garlandlai24@gmail.com>
2020-05-26 23:31:00 -0700

    create Burger.js and css file to render created Burger

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import classes from './burgerIngredient.module.css';
+import classes from './BurgerIngredients.module.css';
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
