8033339063688d222891d317b42fd88a0298a115
Garland Lai <garlandlai24@gmail.com>
2020-05-27 11:58:25 -0700

    Add BuildControl className to div to get styling to work

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './BuildControl.module.css';
 
 const buildControl = (props) => (
-	<div>
+	<div className={classes.BuildControl}>
 		<div className={classes.Label}>{props.label}</div>
 		<button className={classes.Less}>Less</button>
 		<button className={classes.More}>More</button>
