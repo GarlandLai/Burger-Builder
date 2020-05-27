bfa637558e0cbb53e8d868629d4bce778e1f56a9
Garland Lai <garlandlai24@gmail.com>
2020-05-26 22:50:52 -0700

    create Layout css file. Note, we must add 'module' to css name since its built into the new react scripts without having to eject. Also create BurgerBuilder folder/files

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
 	<Aux>
 		<div>Toolbar, SideDrawer, BackDrop</div>
-		<main>{props.children}</main>
+		<main className={classes.Content}>{props.children}</main>
 	</Aux>
 );
 
