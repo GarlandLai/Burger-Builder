cc2638f97a6d8afdd0ff6401977f9f99c913e8a1
Garland Lai <garlandlai24@gmail.com>
2020-05-27 11:02:16 -0700

    add state to burger builder and dynamically display ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+		return [...Array(props.ingredients[igKey])].map((_, i) => {
+			return <BurgerIngredient key={igKey + i} type={igKey} />;
+		});
+	});
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
