1b52841ed5a9f31dbc09165c3ec6542a141b13cd
Garland Lai <garlandlai24@gmail.com>
2020-05-28 13:10:47 -0700

    add handler to remove ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,13 +37,30 @@ class BurgerBuilder extends Component {
 		});
 	};
 
-	removeIngredientHandler = (type) => {};
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients,
+		};
+		updatedIngredients[type] = updatedCount;
+		const priceAddition = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceAddition;
+		this.setState({
+			totalPrice: newPrice,
+			ingredients: updatedIngredients,
+		});
+	};
 
 	render() {
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls ingredientAdded={this.addIngredientHandler} />
+				<BuildControls
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+				/>
 			</Aux>
 		);
 	}
