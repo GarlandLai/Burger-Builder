6aaff3fdd09414a19d2e88ccb027346e44ef1cfb
Garland Lai <garlandlai24@gmail.com>
2020-05-28 13:05:23 -0700

    add handler to add ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,14 +5,18 @@ import BuildControl from './BuildControl/BuildControl';
 const controls = [
 	{ label: 'Salad', type: 'salad' },
 	{ label: 'Bacon', type: 'bacon' },
-	{ label: 'Cheese', type: 'Cheese' },
+	{ label: 'Cheese', type: 'cheese' },
 	{ label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
 	<div className={classes.BuildControls}>
 		{controls.map((ctrl) => (
-			<BuildControl key={ctrl.label} label={ctrl.label} />
+			<BuildControl
+				key={ctrl.label}
+				label={ctrl.label}
+				added={() => props.ingredientAdded(ctrl.type)}
+			/>
 		))}
 	</div>
 );
