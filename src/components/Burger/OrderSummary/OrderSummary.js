362b49247958d2449bab47fb63faef3eeb4afbf4
Garland Lai <garlandlai24@gmail.com>
2020-05-29 15:07:33 -0700

    add reusable button component with css classes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,8 @@ const orderSummary = (props) => {
 			<p>A delicious burger with the following ingredients:</p>
 			<ul>{ingredientSummary}</ul>
 			<p>Continue to Checkout?</p>
+			<button>CANCEL</button>
+			<button>CONTINUE</button>
 		</Aux>
 	);
 };
