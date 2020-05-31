85c74f89b026654fbf471804b61e67865a204fe2
Garland Lai <garlandlai24@gmail.com>
2020-05-30 18:48:47 -0700

    add navigation items and item inside toolbar

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import classes from './NavigationItem.module.css';
 
-const navigationItem = () => (
-	<li>
-		<a href="/">A Link</a>
+const navigationItem = (props) => (
+	<li className={classes.NavigationItem}>
+		<a href={props.link} className={props.active ? classes.active : null}>
+			{props.children}
+		</a>
 	</li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => <ul></ul>;
+const navigationItems = () => (
+	<ul className={classes.NavigationItems}>
+		<NavigationItem link="/" active>
+			Burger Builder
+		</NavigationItem>
+		<NavigationItem link="/">Checkout</NavigationItem>
+	</ul>
+);
 
 export default navigationItems;
