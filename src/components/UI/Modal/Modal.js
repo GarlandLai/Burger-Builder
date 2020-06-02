6f851fb4637586b6ba775418639cf6f206de94e2
Garland Lai <garlandlai24@gmail.com>
2020-06-02 13:15:01 -0700

    optimize app to not unnecessary update modal/ordersummary unless needed

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-	<Aux>
-		<Backdrop show={props.show} clicked={props.modalClosed} />
-		<div
-			className={classes.Modal}
-			style={{
-				transform: props.show ? 'transformY(0)' : 'transformY(-100vh)',
-				opacity: props.show ? '1' : '0',
-				pointerEvents: props.show ? 'inherit' : 'none',
-			}}
-		>
-			{props.children}
-		</div>
-	</Aux>
-);
-export default modal;
+class Modal extends Component {
+	// This make sure that props.show is different before updating. this also controls update of child (orderSummary)
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
+
+	componentDidUpdate() {
+		console.log('Modal Will update');
+	}
+
+	render() {
+		return (
+			<Aux>
+				<Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+				<div
+					className={classes.Modal}
+					style={{
+						transform: this.props.show ? 'transformY(0)' : 'transformY(-100vh)',
+						opacity: this.props.show ? '1' : '0',
+						pointerEvents: this.props.show ? 'inherit' : 'none',
+					}}
+				>
+					{this.props.children}
+				</div>
+			</Aux>
+		);
+	}
+}
+export default Modal;
