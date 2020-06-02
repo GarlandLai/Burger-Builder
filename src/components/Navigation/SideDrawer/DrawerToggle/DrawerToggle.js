cde2eb7a99e19c4f377940d3f2e3d5ad5d24a8d1
Garland Lai <garlandlai24@gmail.com>
2020-06-02 12:20:03 -0700

    add custom css for menu tab

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import classes from './DrawerToggle.module.css';
 
-const drawerToggle = (props) => <div onClick={props.clicked}> Menu </div>;
+const drawerToggle = (props) => (
+	<div className={classes.DrawerToggle} onClick={props.clicked}>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+);
 
 export default drawerToggle;
