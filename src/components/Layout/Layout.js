c932d30807ebc688e30f4eafb55bb204ca159359
Garland Lai <garlandlai24@gmail.com>
2020-06-02 12:08:09 -0700

    add toggle functionality for menu on tool bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,16 +6,23 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class layout extends Component {
 	state = {
-		showSideDrawer: true,
+		showSideDrawer: false,
 	};
 
 	sideDrawerClosedHandler = () => {
 		this.setState({ showSideDrawer: false });
 	};
+
+	sideDrawToggleHandler = () => {
+		this.setState((prevState) => {
+			return { showSideDrawer: !this.state.showSideDrawer };
+		});
+	};
+
 	render() {
 		return (
 			<Aux>
-				<Toolbar />
+				<Toolbar drawerToggle={this.sideDrawToggleHandler} />
 				<SideDrawer
 					open={this.state.showSideDrawer}
 					closed={this.sideDrawerClosedHandler}
