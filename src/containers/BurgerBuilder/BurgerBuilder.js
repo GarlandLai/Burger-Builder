4c319ded395f8f379b0db92f66e7094ec2e92304
Garland Lai <garlandlai24@gmail.com>
2020-06-10 12:38:22 -0700

    post an order with customer info to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -81,7 +82,25 @@ class BurgerBuilder extends Component {
 	};
 
 	purchaseContinueHandler = () => {
-		alert('Continue');
+		// alert('Continue');
+		const order = {
+			ingredient: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'Garland Lai',
+				address: {
+					street: 'Random street',
+					zipCode: '94111',
+					country: 'US',
+				},
+				email: 'email@test.com',
+			},
+			deliveryMethod: 'fastest',
+		};
+		axios
+			.post('/orders.json', order)
+			.then((response) => console.log(response))
+			.catch((error) => console.log(error));
 	};
 
 	render() {
