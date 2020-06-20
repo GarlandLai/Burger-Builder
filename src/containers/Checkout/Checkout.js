3644a259324f6628d36182d9e4befc3c67d12270
Garland Lai <garlandlai24@gmail.com>
2020-06-20 11:29:48 -0700

    create ContactData.js to display form when user continues on checkout page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 	// Will be repalced since these are made up data for now
@@ -36,6 +39,10 @@ class Checkout extends Component {
 					ingredients={this.state.ingredients}
 					checkoutCancelled={this.checkoutCancelledHandler}
 					checkoutContinued={this.checkoutContinuedHandler}
+				/>
+				<Route
+					path={this.props.match.path + '/contact-data'}
+					component={ContactData}
 				/>
 			</div>
 		);
