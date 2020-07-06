6b5c5bf61cd903eeafa904f5ba9773fc6502f42e
Garland Lai <garlandlai24@gmail.com>
2020-07-06 11:35:22 -0700

    add logic to add or reduce ingredient in reducer

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,12 +26,13 @@ class BurgerBuilder extends Component {
 	};
 
 	componentDidMount() {
-		axios
-			.get('https://react-my-burger-a16da.firebaseio.com/ingredients.json')
-			.then((response) => this.setState({ ingredients: response.data }))
-			.catch((error) => {
-				this.setState({ error: true });
-			});
+		console.log(this.props);
+		// axios
+		// 	.get('https://react-my-burger-a16da.firebaseio.com/ingredients.json')
+		// 	.then((response) => this.setState({ ingredients: response.data }))
+		// 	.catch((error) => {
+		// 		this.setState({ error: true });
+		// 	});
 	}
 
 	updatePurchaseState = (ingredients) => {
