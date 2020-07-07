b990d046fcc288c373226e8506432d39c28a2135
Garland Lai <garlandlai24@gmail.com>
2020-07-07 11:39:03 -0700

    activate order button by changing how we handle the purchasable state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,7 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
 	state = {
-		purchasable: false,
+		// purchasable: false,
 		purchasing: false,
 		loading: false,
 		error: false,
@@ -37,7 +37,7 @@ class BurgerBuilder extends Component {
 			.reduce((sum, el) => {
 				return sum + el;
 			}, 0);
-		this.setState({ purchasable: sum > 0 });
+		return sum > 0;
 	};
 
 	// addIngredientHandler = (type) => {
@@ -131,7 +131,7 @@ class BurgerBuilder extends Component {
 						ingredientAdded={this.props.onIngredientAdded}
 						ingredientRemoved={this.props.onIngredientRemoved}
 						disabled={disabledInfo}
-						purchasable={this.state.purchasable}
+						purchasable={this.updatePurchaseState(this.props.ings)}
 						ordered={this.purchaseHandler}
 						price={this.props.price}
 					/>
