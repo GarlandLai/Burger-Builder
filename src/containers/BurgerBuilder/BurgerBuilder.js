7397c8a0257f6eb12b72bcdb3e109ec4eb292194
Garland Lai <garlandlai24@gmail.com>
2020-07-13 14:16:55 -0700

    installl redux thunk and update index.js to handle async code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,12 +21,12 @@ class BurgerBuilder extends Component {
 
 	componentDidMount() {
 		console.log(this.props);
-		// axios
-		// 	.get('https://react-my-burger-a16da.firebaseio.com/ingredients.json')
-		// 	.then((response) => this.setState({ ingredients: response.data }))
-		// 	.catch((error) => {
-		// 		this.setState({ error: true });
-		// 	});
+		axios
+			.get('https://react-my-burger-a16da.firebaseio.com/ingredients.json')
+			.then((response) => this.setState({ ingredients: response.data }))
+			.catch((error) => {
+				this.setState({ error: true });
+			});
 	}
 
 	updatePurchaseState = (ingredients) => {
