f1838391925f28bd6adcda4054e68e337d57e0d1
Garland Lai <garlandlai24@gmail.com>
2020-07-16 11:24:42 -0700

    add files to create Authenticate page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from '../src/containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
 	render() {
@@ -15,6 +16,7 @@ class App extends Component {
 					<Switch>
 						<Route path="/checkout" component={Checkout} />
 						<Route path="/orders" component={Orders} />
+						<Route path="/auth" component={Auth} />
 						<Route path="/" exact component={BurgerBuilder} />
 					</Switch>
 				</Layout>
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.module.css';
 
 class Auth extends Component {
 	state = {
@@ -12,6 +15,21 @@ class Auth extends Component {
 				value: '',
 				validation: {
 					required: true,
+					isEmail: true,
+				},
+				valid: false,
+				touched: false,
+			},
+			password: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'password',
+					placeholder: 'Password',
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 6,
 				},
 				valid: false,
 				touched: false,
@@ -20,10 +38,35 @@ class Auth extends Component {
 	};
 
 	render() {
+		const formElementArray = [];
+		for (let key in this.state.controls) {
+			formElementArray.push({
+				id: key,
+				config: this.state.controls[key],
+			});
+		}
+
+		const form = formElementArray.map((formElement) => (
+			<Input
+				key={formElement.id}
+				elementType={formElement.config.elementType}
+				elementConfig={formElement.config.elementConfig}
+				value={formElement.config.value}
+				invalid={!formElement.config.valid}
+				shouldValidate={formElement.config.validation}
+				touched={formElement.config.touched}
+				changed={(event) => this.inputChangedHandler(event, formElement.id)}
+			/>
+		));
 		return (
-			<div>
-				<form></form>
+			<div className={classes.Auth}>
+				<form>
+					{form}
+					<Button btnType="Success">Submit</Button>
+				</form>
 			</div>
 		);
 	}
 }
+
+export default Auth;
