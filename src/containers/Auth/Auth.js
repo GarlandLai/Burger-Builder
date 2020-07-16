1b7b0f4285b2959a8f949dc9e542afb457c7fb30
Garland Lai <garlandlai24@gmail.com>
2020-07-16 11:31:04 -0700

    complete Auth form. Found out I have an issue with the check Validity

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -10,7 +10,7 @@ class Auth extends Component {
 				elementType: 'input',
 				elementConfig: {
 					type: 'email',
-					placeholder: 'Mail Address',
+					placeholder: 'Email Address',
 				},
 				value: '',
 				validation: {
@@ -35,6 +35,53 @@ class Auth extends Component {
 				touched: false,
 			},
 		},
+	};
+
+	checkValidity(value, rules) {
+		let isValid = true;
+		if (!rules) {
+			return true;
+		}
+
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength && isValid;
+		}
+
+		if (rules.isEmail) {
+			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+		if (rules.isNumeric) {
+			const pattern = /^\d+$/;
+			isValid = pattern.test(value) && isValid;
+		}
+
+		return isValid;
+	}
+
+	inputChangedHandler = (event, controlName) => {
+		const updatedControls = {
+			...this.state.controls,
+			[controlName]: {
+				...this.state.controls[controlName],
+				value: event.target.value,
+				valid: this.checkValidity(
+					event.target.value,
+					this.state.controls[controlName]
+				),
+				touched: true,
+			},
+		};
+		this.setState({ controls: updatedControls });
 	};
 
 	render() {
