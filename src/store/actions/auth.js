d2049f6f0d06219f02135b14b34fe845ea0345fe
Garland Lai <garlandlai24@gmail.com>
2020-07-16 18:45:01 -0700

    add users to firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
 	return {
@@ -23,5 +24,23 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
 	return (dispatch) => {
 		dispatch(authStart());
+		const authData = {
+			email: email,
+			password: password,
+			returnSecureToken: true,
+		};
+		axios
+			.post(
+				'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDYKE6TIHNQZQDclQ5il_21mq7Ax81h15w',
+				authData
+			)
+			.then((response) => {
+				console.log(response);
+				dispatch(authSuccess(response.data));
+			})
+			.catch((err) => {
+				console.log(err);
+				dispatch(authFail(err));
+			});
 	};
 };
