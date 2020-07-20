14ae358d99754b90214dae9af1bb9be88cc1f262
Garland Lai <garlandlai24@gmail.com>
2020-07-20 11:26:56 -0700

    add spinner and error message

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -44,7 +44,7 @@ export const auth = (email, password, isSignup) => {
 			})
 			.catch((err) => {
 				console.log(err);
-				dispatch(authFail(err));
+				dispatch(authFail(err.response.data.error));
 			});
 	};
 };
