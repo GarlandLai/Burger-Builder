3c35855fa19df945013f580d40b7b4c5952e20e7
Garland Lai <garlandlai24@gmail.com>
2020-07-22 11:50:31 -0700

    fix issues so user stays logged in if they have a valid token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -90,14 +90,15 @@ export const authCheckState = () => {
 			dispatch(logout());
 		} else {
 			const expirationDate = new Date(localStorage.getItem('expirationDate'));
-			if (expirationDate > new Date()) {
+			if (expirationDate <= new Date()) {
 				dispatch(logout());
 			} else {
 				const userId = localStorage.getItem('userId');
 				dispatch(authSuccess(token, userId));
 				dispatch(
+					// Get seconds
 					checkAuthTimeout(
-						expirationDate.getSeconds() - new Date().getSeconds()
+						(expirationDate.getTime() - new Date().getTime()) / 1000
 					)
 				);
 			}
