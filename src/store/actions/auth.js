68e20840c3f80854d39297a8d45971535f94b275
Garland Lai <garlandlai24@gmail.com>
2020-07-22 11:28:44 -0700

    create persistant auth state with local storage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,10 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+	// Remove from local storage
+	localStorage.removeItem('token');
+	localStorage.removeItem('expirationDate');
+	localStorage.removeItem('localId');
 	return {
 		type: actionTypes.AUTH_LOGOUT,
 	};
@@ -54,6 +58,14 @@ export const auth = (email, password, isSignup) => {
 			.post(url, authData)
 			.then((response) => {
 				console.log(response);
+				// save in local storage
+				const expirationDate = new Date(
+					new Date().getTime() + response.data.expiresIn * 1000
+				);
+				console.log('expiration', expirationDate);
+				localStorage.setItem('token', response.data.idToken);
+				localStorage.setItem('expirationDate', expirationDate);
+				localStorage.setItem('userId', response.data.localId);
 				dispatch(authSuccess(response.data.idToken, response.data.localId));
 				dispatch(checkAuthTimeout(response.data.expiresIn));
 			})
@@ -68,5 +80,27 @@ export const setAuthRedirectPath = (path) => {
 	return {
 		type: actionTypes.SET_AUTH_REDIRECT_PATH,
 		path: path,
+	};
+};
+
+export const authCheckState = () => {
+	return (dispatch) => {
+		const token = localStorage.getItem('token');
+		if (!token) {
+			dispatch(logout());
+		} else {
+			const expirationDate = new Date(localStorage.getItem('expirationDate'));
+			if (expirationDate > new Date()) {
+				dispatch(logout());
+			} else {
+				const userId = localStorage.getItem('userId');
+				dispatch(authSuccess(token, userId));
+				dispatch(
+					checkAuthTimeout(
+						expirationDate.getSeconds() - new Date().getSeconds()
+					)
+				);
+			}
+		}
 	};
 };
