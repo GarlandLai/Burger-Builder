475e6c499411e548fb4d6d7d92165b7628334899
Garland Lai <garlandlai24@gmail.com>
2020-07-22 13:18:37 -0700

    add some route pertection with guards in front end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -16,22 +16,39 @@ class App extends Component {
 	}
 
 	render() {
+		let routes = (
+			<Switch>
+				<Route path="/auth" component={Auth} />
+				<Route path="/" exact component={BurgerBuilder} />
+				<Redirect to="/" />
+			</Switch>
+		);
+
+		if (this.props.isAuthenticated) {
+			routes = (
+				<Switch>
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/orders" component={Orders} />
+					<Route path="/logout" component={Logout} />
+					<Route path="/" exact component={BurgerBuilder} />
+					<Redirect to="/" />
+				</Switch>
+			);
+		}
+
 		return (
 			<div>
-				<Layout>
-					{/* Could use switch or just exact. Both shown for below but not needed */}
-					<Switch>
-						<Route path="/checkout" component={Checkout} />
-						<Route path="/orders" component={Orders} />
-						<Route path="/auth" component={Auth} />
-						<Route path="/logout" component={Logout} />
-						<Route path="/" exact component={BurgerBuilder} />
-					</Switch>
-				</Layout>
+				<Layout>{routes}</Layout>
 			</div>
 		);
 	}
 }
+
+const mapStateToProps = (state) => {
+	return {
+		isAuthenticated: state.auth.token !== null,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -39,4 +56,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
