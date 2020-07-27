d26fb169fe60eaea443410c788b6f8f448a34a19
Garland Lai <garlandlai24@gmail.com>
2020-07-27 14:24:58 -0700

    clean up some last console logs found when going through the order process

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -145,7 +145,6 @@ class ContactData extends Component {
 		for (let inputIdentifier in updatedOrderForm) {
 			formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
 		}
-		console.log('formIsValid', formIsValid);
 		this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
 	};
 
