5f1cd614fab9a63e6d3e4a3165c87ec8dc77ec13
Garland Lai <garlandlai24@gmail.com>
2020-07-31 12:05:05 -0700

    add test and use the contains method

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -25,4 +25,11 @@ describe('<NavigationItems />', () => {
 		wrapper.setProps({ isAuthenticated: true });
 		expect(wrapper.find(NavigationItem)).toHaveLength(3);
 	});
+
+	it('should include the navigation item with link and logout text if authenticated', () => {
+		wrapper.setProps({ isAuthenticated: true });
+		expect(
+			wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+		).toEqual(true);
+	});
 });
