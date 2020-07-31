9e4a654067bfeb5301e90c02a47e64f2adaaa518
Garland Lai <garlandlai24@gmail.com>
2020-07-30 21:59:45 -0700

    write second test and also refracter to use several helper functions

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,8 +7,22 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<NavigationItems />);
+	});
+
 	it('should render two navigation items elements if not authenticated', () => {
-		const wrapper = shallow(<NavigationItems />);
+		// Use the beforeEach instead
+		// const wrapper = shallow(<NavigationItems />);
 		expect(wrapper.find(NavigationItem)).toHaveLength(2);
+	});
+
+	it('should render three navigation items elements if authenticated', () => {
+		// Could set below without const
+		// wrapper = shallow(<NavigationItems isAuthenticated />);
+		// or use this helper function
+		wrapper.setProps({ isAuthenticated: true });
+		expect(wrapper.find(NavigationItem)).toHaveLength(3);
 	});
 });
